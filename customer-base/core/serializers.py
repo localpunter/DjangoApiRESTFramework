c28072e0ca3db1aacc27da3ea2deb53a17df1905
localpunter <localpunter@sky.com>
2019-03-26 15:45:22 +0000

    Handling ForeignKey and OneToOne relationshps

--- customer-base/core/serializers.py
+++ customer-base/core/serializers.py
@@ -13,8 +13,20 @@ class DocumentSerializer(serializers.ModelSerializer):
             'id',
             'dtype',
             'doc_number',
-            'customer',
+            'customer'
             )
+
+class DocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Document
+        fields = (
+            'id',
+            'dtype',
+            'doc_number',
+            'customer'
+            )
+
+        read_only_fields = ['customer']
 
 
 class DataSheetSerializer(serializers.ModelSerializer):
@@ -23,7 +35,7 @@ class DataSheetSerializer(serializers.ModelSerializer):
         fields = (
             'id',
             'description',
-            'historical_data',
+            'historical_data'
             )
 
 
@@ -39,9 +51,9 @@ class ProfessionSerializer(serializers.ModelSerializer):
 # Serializers define the API representation.
 class CustomerSerializer(serializers.ModelSerializer):
     number_of_professions = serializers.SerializerMethodField()
-    data_sheet = DataSheetSerializer(read_only=True)
+    data_sheet = DataSheetSerializer()
     professions = ProfessionSerializer(many=True)
-    document_set = DocumentSerializer(many=True, read_only=True)
+    document_set = DocumentSerializer(many=True)
 
     class Meta:
         model = Customer
@@ -54,14 +66,29 @@ class CustomerSerializer(serializers.ModelSerializer):
             'active',
             'status_message',
             'number_of_professions',
-            'document_set',
+            'document_set'
             )
 
     def create(self, validated_data):
         professions = validated_data['professions']
         del validated_data['professions']
 
+        document_set = validated_data['document_set']
+        del validated_data['document_set']
+
+        data_sheet = validated_data['data_sheet']
+        del validated_data['data_sheet']
+
+        d_sheet = DataSheet.objects.create(**data_sheet)
         customer = Customer.objects.create(**validated_data)
+        customer.data_sheet = d_sheet
+
+        for doc in document_set:
+            Document.objects.create(
+                dtype=doc['dtype'],
+                doc_number=doc['doc_number'],
+                customer_id=customer.id
+            )
 
         for profession in professions:
             prof = Profession.objects.create(**profession)
